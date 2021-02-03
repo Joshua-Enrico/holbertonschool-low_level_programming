063772357267dc0f3c1a83d426bb1f27c33cffa6
yoyogold-a11 <tmrfack@gmail.com>
2021-02-03 22:46:54 +0000

    almost done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int li;
 long long int lli;
 float flt;
 
-printf("Size of char: %lu bytes(s)\n", sizeof(chr));
-printf("Size of int: %lu bytes(s)\n", sizeof(tni));
-printf("Size of long int: %lu bytes(s)\n", sizeof(li));
-printf("Size of long long init: %lu bytes(s)\n", sizeof(lli));
-printf("Size of float: %lu bytes(s)\n", sizeof(flt));
+printf("Size of a char: %lu bytes(s)\n", sizeof(chr));
+printf("Size of a int: %lu bytes(s)\n", sizeof(tni));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
+printf("Size of a long long init: %lu bytes(s)\n", sizeof(lli));
+printf("Size of a float: %lu bytes(s)\n", sizeof(flt));
 
 return (0);
 }
