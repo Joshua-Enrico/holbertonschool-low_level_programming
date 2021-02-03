0d714d3510821f5e32cff9caa3ba94d246813e7f
yoyogold-a11 <tmrfack@gmail.com>
2021-02-03 19:58:58 +0000

    fixing bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *Function : Prints "Programming is like
  *building a multilingual puzzle using puts function
  *retunr: 0 if the programming ran to completion
-*/
+ */
 int main(void)
 {
-   puts("\"programing is like building a multilingual puzzle");
-   return (0);
+puts("\"programing is like building a multilingual puzzle");
+return (0);
 }
