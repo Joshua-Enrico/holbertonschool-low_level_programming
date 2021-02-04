6ea33693208fc8ae9a46453ffc71a94ffcafa2b5
yoyogold-a11 <tmrfack@gmail.com>
2021-02-04 21:33:59 +0000

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: random numbers with conditions
+ *Return: 0 Always
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z' ; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z' ; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
