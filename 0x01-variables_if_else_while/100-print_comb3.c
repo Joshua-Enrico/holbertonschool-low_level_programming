6bb483c78a9c04844794e133b222e1c9853e32ae
yoyogold-a11 <tmrfack@gmail.com>
2021-02-05 04:03:58 +0000

    variable

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,21 +3,23 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Description: from 00 to 99 with some spesifications
+ *Description: from 00 to 99 with some specifications
  *Return: 0 Always
  */
 int main(void)
 {
 int a, b;
-for (a = '0'; a <= '9' ; a++)
+
+for (a = '0'; a <= '9'; a++)
 {
-for (b = '0'; b <= '9' ; b++)
+for (b = '0'; b <= '9'; b++)
 {
 if (a < b)
 {
 putchar(a);
 putchar(b);
-if (a != '9' || b != '9')
+
+if (a != '8' || (a == '8' && b != '9'))
 {
 putchar(',');
 putchar(' ');
@@ -25,6 +27,8 @@ putchar(' ');
 }
 }
 }
+
 putchar('\n');
+
 return (0);
 }
