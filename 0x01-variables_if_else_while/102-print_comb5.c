f9aea132c91d4fb4246a90b6d95ec257b5e96057
yoyogold-a11 <tmrfack@gmail.com>
2021-02-05 09:44:03 +0000

    variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,36 +3,44 @@
 /**
   * main - Prints 3 combination of numbers
   *
-  * Return: Always (Success)
+  * Return:0
   */
 int main(void)
 {
-int a, b, c, d;
-for (a = 48; a <= 57; a++)
-{
-for (b = 48; b <= 57; b++)
-{
-for (c = 48; c <= 57; c++)
-{
-for (d = 48; d <= 57; d++)
-{
-if (a < c || (a == c && b < d))
-{
-putchar(a);
-putchar(b);
-putchar(' ');
-putchar(c);
-putchar(d);
-if (!(a == 9 && b == 8))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int a, b, c, d;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (((c + d) > (a+ b) &&  c >= a) || a < c)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+
+					if (c + b + c + d == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
