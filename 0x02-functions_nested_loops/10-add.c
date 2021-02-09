65efd4a358492243d1c8f4ed4e5c9f0999dc1712
yoyogold-a11 <tmrfack@gmail.com>
2021-02-09 17:29:05 +0000

    add two

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  *add - Adds two vlues.
+  *@a: The first number
+  *@b: The second one
+  *
+  *Return: integer value
+  */
+int add(int a, int b)
+{
+return (a + b);
+}
