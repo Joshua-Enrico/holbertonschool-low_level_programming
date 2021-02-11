60d07b38b6d32169eeb2aa1e3d9dee51cd44ee39
yoyogold-a11 <tmrfack@gmail.com>
2021-02-11 19:32:46 +0000

    FizzBuzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - FizzBuzz game
+*
+*Return: Value
+*/
+int main(void)
+{
+int a = 1;
+while (a <= 100)
+{
+if (a % 5 == 0 && a % 3 == 0)
+{
+printf("FizzBuzz");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz");
+}
+else
+printf("%d", a);
+printf(" ");
+a++;
+}
+printf("\n");
+return (0);
+}
