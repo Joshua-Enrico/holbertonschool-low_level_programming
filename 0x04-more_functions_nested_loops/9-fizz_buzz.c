b98131687220e20412c7b99cafb0cf669f5e3c14
yoyogold-a11 <tmrfack@gmail.com>
2021-02-11 19:49:01 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,28 @@
 */
 int main(void)
 {
-	int i;
+int a = 1;
+while (a <= 100)
+{
+if (a % 5 == 0 && a % 3 == 0)
 
-	i = 1;
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d", i);
+printf("FizzBuzz");
 
-		if (i != 100)
-			printf(" ");
-		i++;
-	}
-	printf("\n");
+else if (a % 5 == 0)
 
-	return (0);
+printf("Buzz");
+
+else if (a % 3 == 0)
+
+printf("Fizz");
+
+else
+printf("%d", a);
+
+if (a !=100)
+printf(" ");
+a++;
+}
+printf("\n");
+return (0);
 }
