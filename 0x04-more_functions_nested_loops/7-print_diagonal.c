dbf590d71554f7c473daa9fa048974f6d0ed9f83
yoyogold-a11 <tmrfack@gmail.com>
2021-02-11 17:11:23 +0000

    prints line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+*print_diagonal - prints line.
+*@n: value
+*Return:value
+*/
+void print_diagonal(int n)
+{
+int c, i;
+
+c = 0;
+
+while (n > 0)
+{
+i = c;
+while (i > 0)
+{
+_putchar(' ');
+i--;
+}
+_putchar('\\');
+_putchar('\n');
+c++;
+n--;
+}
+if (c < 1)
+_putchar('\n');
+}
