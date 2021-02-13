59751b2cec8325707186fb2d9b8cd67392eb790d
yoyogold-a11 <tmrfack@gmail.com>
2021-02-13 03:29:11 +0000

    Prints a integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,41 @@
   */
 void print_number(int n)
 {
-unsigned int k = n;
-if (n < 0)
+long m; /* power of 10 */
+int c; /* boolean check */
+long num; /* convert int to long */
+
+num = n;
+
+if (num < 0)
 {
-n *= -1;
-k = n;
+num *= -1;
 _putchar('-');
 }
 
-k /= 10;
 
-if (k != 0)
-print_number(k);
+m = 1;
+c = 1;
+while (c)
+{
+if (num / (m * 10) > 0)
+m *= 10;
+else
+c = 0;
+}
 
-_putchar((unsigned int) n % 10 + '0');
 
+while (num >= 0)
+{
+if (m == 1)
+{
+_putchar(num % 10 + '0');
+num = -1;
+}
+else
+{
+_putchar((num / m % 10) + '0');
+m /= 10;
+}
+}
 }
