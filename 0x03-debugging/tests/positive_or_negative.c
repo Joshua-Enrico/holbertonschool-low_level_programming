524e2abb15a36cfc1a6596bbc7f5238380587350
yoyogold-a11 <tmrfack@gmail.com>
2021-02-13 03:58:17 +0000

    tests

---
+++ 0x03-debugging/tests/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+
+i = 0;
+if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+}
