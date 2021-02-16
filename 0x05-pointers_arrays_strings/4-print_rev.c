84427d2f80299de4ba0671e1cc36ff5bc3f599ba
yoyogold-a11 <tmrfack@gmail.com>
2021-02-16 20:27:08 +0000

    prints reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *  print_rev - Prints invert
+ * description: print_rev
+ *@s: pointer to s
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+   
+int a;
+a = 0;
+
+while (*s != 0)
+{
+
+s++;
+a++;
+}
+s--;
+while(a - 1 >= 0)
+{
+    _putchar(*s);
+    s--;
+    a--;
+}
+_putchar('\n');
+}
