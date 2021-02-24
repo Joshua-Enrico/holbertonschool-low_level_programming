911527bd75953539fcf6d24a3de65d27851f74be
yoyogold-a11 <tmrfack@gmail.com>
2021-02-24 21:48:53 +0000

    palindrome

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,3 +9,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int _sqrt_rec(int n, int x);
 int is_prime(int n, int x);
+int palindrome(char *s, int first, int last);
+int _strlen_recursion(char *s);
