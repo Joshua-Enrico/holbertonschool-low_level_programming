011889ea84107315e5bd4995a4ad1b9c4fd26a36
yoyogold-a11 <tmrfack@gmail.com>
2021-02-25 15:56:36 +0000

    compares strings

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * wildcmp -  compare two strings if identical or not.
+ * @s1: char type
+ * @s2: char type
+ * Return: return 1 or 0.
+ **/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+return (wildcmp(s1 + 1, s2 + 1));
+
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+
+if (*s2 == '*')
+{
+if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+return (1);
+}
+return (0);
+}
