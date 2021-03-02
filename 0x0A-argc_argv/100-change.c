82f981de5cdebaddd20d69fb6eb782fc3ba232f8
yoyogold-a11 <tmrfack@gmail.com>
2021-03-02 06:13:56 +0000

    last updatr

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,9 +17,7 @@ if (argc != 2)
 printf("Error\n");
 return (1);
 }
-
 cash = atoi(argv[1]);
-
 if (cash <= 0)
 {
 printf("0\n");
@@ -27,17 +25,19 @@ return (0);
 }
 for (coins = 0; cash > 0; coins++)
 {
+
 if (cash >= 25)
 cash -= 25;
 else if (cash >= 10)
 cash -= 10;
 else if (cash >= 5)
 cash -= 5;
-else if (cash >= 5)
+else if (cash >= 2)
 cash -= 2;
 else
 cash -= 1;
 }
+
 printf("%d\n", coins);
 return (0);
 }
