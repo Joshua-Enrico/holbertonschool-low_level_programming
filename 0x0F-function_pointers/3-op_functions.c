8d6009fc73e42e08f12244372ce49b226b3deccb
yoyogold-a11 <tmrfack@gmail.com>
2021-03-11 01:11:36 +0000

    fixing code

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,6 +41,11 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(0);
+}
 return (a / b);
 }
 /**
@@ -51,5 +56,10 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(0);
+}
 return (a % b);
 }
