67a0ff7db15bc30ef48c0ebfb7292b33e5c8675e
yoyogold-a11 <tmrfack@gmail.com>
2021-03-11 00:20:06 +0000

    basic calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Desc: main function
+ * @argc: argument count
+ * @argv: argumemnts
+ * Return: Main function return 0
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, c;
+int (*x)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+x = get_op_func(argv[2]);
+
+if (x == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+c = x(a, b);
+printf("%d\n", c);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - Entry point
+ * @a: int type
+ * @b: int type
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+
+/**
+ * op_sub - Entry point
+ * @a: int type
+ * @b: int type
+ * Return: substract
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - Entry point
+ * @a: int type
+ * @b: int type
+ * Return: multiplies
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - Entry point
+ * @a: int type
+ * @b: int type
+ * Return: divides
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod- Entry point
+ * @a: int type
+ * @b: int type
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
