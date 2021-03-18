f0acbb216cf776a83f69b17df11b31f61821630a
yoyogold-a11 <tmrfack@gmail.com>
2021-03-18 18:31:56 +0000

    second test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,40 +3,45 @@
 #include <string.h>
 
 /**
- * strlen - encuentra la longitud de un string
+ * _strlen - encuentra la longitud de un string
  * @str: string de donde se sacara la longitud
- * 
+ *
  * Return: Retorna la longitud del string.
  */
 
 unsigned int _strlen(char *str)
 {
-    int c = 0;
-while(*str)
-{
-    str++;
-    c++;
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
-   
-    return (c);
-}
+/**
+ * add_node - adds a node to the beginning of a linked list
+ * @head: double pointer to a lined list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    if (str == NULL) /*primer caso de nulo*/
-        return (NULL);
+	list_t *new;
 
-    /*definimos espacio de memoria de list_t a new*/
-    new = malloc(sizeof(list_t));
-    if (new == NULL) /*caso de nulo para new*/
-        return (NULL);
+	if (str == NULL) /*primer caso de nulo*/
+		return (NULL);
 
-    new->str = strdup(str); /*definimos espacio de memoria a new->new*/
-    if (new->str == NULL)   /*caso nulo par new->str*/
-        return (NULL);
-    new->len = _strlen(new->str);
-    new->next = *head;
-    *head = new;
-    return (new);
+	/*definimos espacio de memoria de list_t a new*/
+	new = malloc(sizeof(list_t));
+	if (new == NULL) /*caso de nulo para new*/
+		return (NULL);
+
+	new->str = strdup(str); /*definimos espacio de memoria a new->new*/
+	if (new->str == NULL)   /*caso nulo par new->str*/
+		return (NULL);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
