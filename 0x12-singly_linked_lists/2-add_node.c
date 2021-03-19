18c9af0f961655e71eee8704f75e51a15c450009
yoyogold-a11 <tmrfack@gmail.com>
2021-03-19 05:39:59 +0000

    testing

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -33,12 +33,13 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 
 	if (new == NULL)
-		free(new);
+
 		return (NULL);
 
 	new->str = strdup(str);
 	if (new->str == NULL)
-	free(new->str);
+
+
 	return (NULL);
 
 	new->len = _strlen(new->str);
