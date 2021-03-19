fa2d74cc9633e0106442d8a9084734d931c47825
yoyogold-a11 <tmrfack@gmail.com>
2021-03-19 05:33:04 +0000

    testing

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -32,7 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
