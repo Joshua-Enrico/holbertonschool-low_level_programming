9d95510746558300d635cad4ce828213d071126c
yoyogold-a11 <tmrfack@gmail.com>
2021-05-01 19:35:13 +0000

    fixing code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - Print all elements from a list
@@ -6,10 +7,9 @@
  *
  * Return: number of elements
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
