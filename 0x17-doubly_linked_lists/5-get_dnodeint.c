3dd29ce79c22ebd80c417feb5324c336a739f637
yoyogold-a11 <tmrfack@gmail.com>
2021-05-01 20:00:32 +0000

    fixing code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,25 +8,14 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *copy;
-	unsigned int nodes = 0, counter = 0;
+	unsigned int nodes = 0;
 
 	if (head == NULL)
 		return (NULL);
 	copy = head;
-	while (copy != NULL)
-	{
-		copy = copy->next;
-		counter++;
-	}
-	copy = head;
 	if (index == 0)
 	{
 		return (head);
-	}
-	if (index > counter - 1)
-	{
-		printf("!Node does not exit!\n");
-		exit(1);
 	}
 	while (nodes < index)
 	{
