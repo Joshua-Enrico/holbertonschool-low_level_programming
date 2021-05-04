6086f4806dbf8459f9d01b9ee406bda46fa1b5f4
yoyogold-a11 <tmrfack@gmail.com>
2021-05-04 02:47:56 +0000

    fixing

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-weget -P /tmp https://github.com/yoyogold-a11/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries
-export LD_PRELOAD=/tmp/
+wget -P /tmp https://github.com/yoyogold-a11/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/numbers.o
+export LD_PRELOAD=/tmp/numbers.o
