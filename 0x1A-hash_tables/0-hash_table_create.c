41edac45714d151f9786b32f880a7bc8c2111a8d
yoyogold-a11 <tmrfack@gmail.com>
2021-05-28 15:09:13 +0000

    fixing code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 	unsigned long int index;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->size == 0)
 		return (NULL);
 
 	ht->size = size;
