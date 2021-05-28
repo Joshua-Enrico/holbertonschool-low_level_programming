4f1b19da07d56e69cf91f85861a86a54a3a5e013
yoyogold-a11 <tmrfack@gmail.com>
2021-05-28 15:23:39 +0000

    fixing code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,8 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *new_val;
 	unsigned long int index;
 
-	if (key == NULL || ht->array == NULL || value == NULL ||
-		strlen(key) == 0 || ht->size == 0 || ht == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
