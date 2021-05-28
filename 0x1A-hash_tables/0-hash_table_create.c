76e5b38aff0e7955e04c0228d606eec87801485c
yoyogold-a11 <tmrfack@gmail.com>
2021-05-28 15:02:07 +0000

    fixing code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,23 +6,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
-	unsigned long int index;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-	if (ht == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	/* another assigment: size * sizeof (hash_node_t *) * size*/
-	if (ht->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(ht);
+		free(hash_table);
 		return (NULL);
 	}
-	for (index = 0; index < size; index++)
-	{
-		ht->array[index] = NULL;
-	}
-	return (ht);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
